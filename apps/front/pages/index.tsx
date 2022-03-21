c8f3b7e379c012b3dcbbc7d63081564d5a3610c8
Lim Chun Leng <limchunleng@gmail.com>
2022-03-21 17:28:48 +0900

    Add eslint to front

--- apps/front/pages/index.tsx
+++ apps/front/pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -10,11 +10,9 @@ const Home: NextPage = () => {
         <meta name="description" content="Ichigo Loyalty Program" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.main}>
-        TEST
-      </div>
+      <div className={styles.main}>TEST</div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
